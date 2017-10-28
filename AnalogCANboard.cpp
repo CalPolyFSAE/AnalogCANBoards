2109cddbb4c52199efb0f20ae4b37ab85e5cad1e
Mitchell Myjak <mtmyjak@gmail.com>
2017-10-28 13:20:29 -0700

    No longer need multiple message array

--- AnalogCANboard.cpp
+++
@@ -1,6 +0,0 @@
-#include "AnalogCANboard.hpp"
-
-// Seem to need a reference to at least one of these variables to make it build.
-constexpr SensorCANmod::CANMessage SensorCANmod::message50[];
-constexpr SensorCANmod::CANMessage SensorCANmod::message250[];
-constexpr SensorCANmod::CANMessage SensorCANmod::message1000[];
