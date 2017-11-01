34bf23bf9c78845673dbd741e014087d0b16f50d
Mitchell Myjak <mtmyjak@gmail.com>
2017-10-31 19:10:39 -0700

    Adding analogCANboard.cpp to fix bug

---
+++ AnalogCANboard.cpp
@@ -0,0 +1,5 @@
+#include "AnalogCANboard.hpp"
+// Seem to need a reference to at least one of these variables to make it build.
+constexpr SensorCANmod::CANMessage SensorCANmod::message1000[];
+constexpr SensorCANmod::CANMessage SensorCANmod::message250[];
+constexpr SensorCANmod::CANMessage SensorCANmod::message50[];
