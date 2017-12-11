f4e35378be2f5ce2993f91536d60afc13328deb8
mazda4952 <one.sonb.son@gmail.com>
2017-12-10 17:49:30 -0800

    Set up Config namespaces and added Update() logic

--- SensorManager.cpp
+++ SensorManager.cpp
@@ -9,38 +9,50 @@
 
 #include "Config/CONFIG.h"
 
+//static memory initialization
 Sensor* SensorManager::channels[] = {};
+CANSensorTimer* SensorManager::CANMessageTimers[] = {};
 
-SensorManager::CANMessageTimer* SensorManager::CANMessageTimers[] = {};
-
+//TODO: put configuration data into progmem
 void SensorManager::Init()
 {
     // create timers for CAN message timing
-    for (uint8_t i = 0; i < CANCHANNELS; ++i)
+    for (uint8_t i = 0; i < CANCONFIG::CANCHANNELS; ++i)
     {
-        CANMessageTimers[i] = new CANMessageTimer (
-                SensorDatas[i].CANChan.TimingInterval, SensorDatas[i].CANChan.CANID);
+        CANMessageTimers[i] = new CANSensorTimer (
+                CANCONFIG::CANChannels[i].TimingInterval, CANCONFIG::CANChannels[i].CANID );
     }
 
-    for (uint8_t i = 0; i < SENSORS; ++i)
+    //create sensors
+    //sensors could be part of static memory, but this would require restructuring
+    //the config headers
+    for (uint8_t i = 0; i < SENSORCONFIG::SENSORS; ++i)
     {
-        Sensor* sens = new Sensor (SensorDatas[i]);//create sensor
+        Sensor* sens = new Sensor (SENSORCONFIG::SensorDatas[i]);//create sensor
         channels[i] = sens;
         //figure out which sensors go on which CAN messages
-        for (uint8_t canc = 0; canc < CANCHANNELS; ++canc)
+        for (uint8_t canc = 0; canc < CANCONFIG::CANCHANNELS; ++canc)
         {
-            if (CANMessageTimers[canc]->CANID == SensorDatas[i].CANChan.CANID)
+            if (CANMessageTimers[canc]->CANID == SENSORCONFIG::SensorDatas[i].CANChan.CANID)
             {
-                CANMessageTimers[canc]->registerSensor(sens, SensorDatas[i].CANDataChannel);
+                if(!CANMessageTimers[canc]->registerSensor(sens, SENSORCONFIG::SensorDatas[i].CANDataChannel))
+                {
+                    //TODO: Error message CAN / SERIAL
+                    //or error is triggered in register sensor?
+                }
             }
         }
     }
 }
 
-
+//1000Hz interrupt timing call
 void SensorManager::INT_UpdateTiming()
 {
-
+    //update the CANSensorTimers
+    for(uint8_t i = 0; i < CANCONFIG::CANCHANNELS; ++i)
+    {
+        CANMessageTimers[i]->Update();
+    }
 }
 
 
