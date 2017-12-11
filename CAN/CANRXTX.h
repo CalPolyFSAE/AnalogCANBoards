856d5ff886c82188c4ac180c203a0244a4c5f602
mazda4952 <one.sonb.son@gmail.com>
2017-12-10 17:47:18 -0800

    Added CANRXTX class

--- CAN/CANRXTX.h
+++ CAN/CANRXTX.h
@@ -8,8 +8,17 @@
 #ifndef CAN_CANRXTX_H_
 #define CAN_CANRXTX_H_
 
+#include <stdint.h>
 
+class CANRXTX
+{
+public:
+    typedef union CAN_MSG_ID
+    {
+        uint32_t extended;      //29 bit CAN ID
+        uint16_t standard;      //11 bit CAN ID
+    } CAN_MSG_ID;
 
-
+};
 
 #endif /* CAN_CANRXTX_H_ */
